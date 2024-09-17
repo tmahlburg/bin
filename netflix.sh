bea85664ad0aaae7a638e6d739ce3f3a0bb59340
Till Mahlburg <mahlburg@posteo.de>
2024-09-17 22:59:55 +0200

    little script to resize and move vivaldi pop out windows

---
+++ netflix.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if pgrep sway ; then
+	swaymsg exec "vivaldi-stable --new-window netflix.com"
+	sleep 2
+	wlrctl toplevel focus title:"Netflix - Vivaldi"
+	wtype -M ctrl -k F11 -m ctrl
+	swaymsg floating enable
+	sleep 1
+	wlrctl toplevel focus title:"Netflix - Vivaldi"
+	swaymsg resize set 640 px 360 px
+	swaymsg sticky enable
+else
+	echo "sway isn't running"
+fi
